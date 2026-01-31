ebadb14c1effc24aed33f9a3b23271f1cb00c68e
Deepak7781 <deepak21082005@gmail.com>
2026-01-31 20:35:23 +0530

    update: avoidance_maneuver.m

--- src/avoidance/avoidance_maneuver.m
+++ src/avoidance/avoidance_maneuver.m
@@ -1,12 +1,20 @@
 
 tle_file = '\data\debris\iridium33_deb.tle';
 
-[tca, missDistance, isRequired] = plan_avoidance_maneuver(tle_file);
+mission = mission_definition();
+    
+scenario = createScenario(mission);
+    
+YPSAT = createSat(scenario, mission);
+    
+debris = createDebris(scenario, tle_file);
+
+[tca, missDistance, isRequired] = plan_avoidance_maneuver(YPSAT, debris);
 
 if isRequired
     fprintf("Avoidance Maneuver is Required\n");
 end
 
 
-maneuverTime = tca - minutes(30);
+% maneuverTime = tca - minutes(30);
 
--- src/avoidance/plan_avoidance_maneuver.m
+++ src/avoidance/plan_avoidance_maneuver.m
@@ -1,13 +1,7 @@
 
-function [tca, missDistance, isRequired] = plan_avoidance_maneuver(tle_file)
+function [tca, missDistance, isRequired] = plan_avoidance_maneuver(YPSAT, debris)
 % Function to check if the avoidance maneuver is required or not
-    mission = mission_definition();
-    
-    scenario = createScenario(mission);
-    
-    YPSAT = createSat(scenario, mission);
-    
-    debris = createDebris(scenario, tle_file);
+   
     
     [tca, missDistance] = detect_conjunctions(YPSAT, debris);
     
