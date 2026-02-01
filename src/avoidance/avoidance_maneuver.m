115b15784d4d4c70396459781eedde8e425c46b9
Deepak7781 <deepak21082005@gmail.com>
2026-02-01 08:08:51 +0530

    update: avoidance_maneuver.m

--- src/avoidance/avoidance_maneuver.m
+++ src/avoidance/avoidance_maneuver.m
@@ -18,18 +18,22 @@ end
 
 maneuverTime = tca - minutes(30);
 
-[pos, vel] = states(YPSAT, maneuverTime, "CoordinateFrame","inertial");
+orbitEl = orbitalElements(YPSAT);
 
-vhat = vel/norm(vel);
+% vhat = vel/norm(vel);
+% 
+% detaV = 0.5; % m/s
+% 
+% vel_new = vel + detaV*vhat;
 
-detaV = 0.5; % m/s
-
-vel_new = vel + detaV*vhat;
-
-YPSAT_NEW = satellite(scenario, pos, vel_new, maneuverTime);
-
-[~,~,range_new,t_new] = aer(YPSAT_NEW, deb);
-
-[minDist_new, idx2] = min(range_new);
-
-fprintf("Miss distance AFTER maneuver: %.1f m\n", minDist_new);
+% YPSAT_NEW = satellite(scenario, ...
+%                        pos, ...
+%                        vel_new, ...
+%                        maneuverTime, ...
+%                        "Name", "YPSAT_NEW");
+% 
+% [~,~,range_new,t_new] = aer(YPSAT_NEW, debris);
+% 
+% [minDist_new, idx2] = min(range_new);
+% 
+% fprintf("Miss distance AFTER maneuver: %.1f m\n", minDist_new);
